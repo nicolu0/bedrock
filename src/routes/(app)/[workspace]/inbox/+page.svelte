a4804feb45a5cbe40cdde6acfa5df6e28cb1f461
Andrew Chang <21andrewch@gmail.com>
2026-02-25 15:39:22 -0800

    padding + issues page

--- src/routes/(app)/[workspace]/inbox/+page.svelte
+++ src/routes/(app)/[workspace]/inbox/+page.svelte
@@ -1,5 +1,5 @@
 <div class="space-y-6">
-	<div class="border-b border-neutral-100 px-6 pb-2">
+	<div class="border-b border-neutral-100 px-6 py-2">
 		<h1 class="text-sm font-normal text-neutral-700">Inbox</h1>
 	</div>
 </div>
