104afd439e759e58b41ba200b73b959a62e1b85d
nicolu0 <nicoluo@gmail.com>
2026-03-03 10:42:30 -0800

    fix: ACTUALLY insta load

--- src/routes/(app)/[workspace]/issue/[issue_id]/[issue_name]/+page.server.js
+++ src/routes/(app)/[workspace]/issue/[issue_id]/[issue_name]/+page.server.js
@@ -1,60 +1,9 @@
 // @ts-nocheck
 import { redirect } from '@sveltejs/kit';
-import { supabaseAdmin } from '$lib/supabaseAdmin';
 
-const statusOrder = ['in_progress', 'todo', 'done'];
-const allowedStatuses = new Set(statusOrder);
-
-export const load = async ({ locals, params }) => {
+export const load = async ({ locals }) => {
 	if (!locals.user) {
 		throw redirect(303, '/');
 	}
-
-	const issueId = params.issue_id;
-	const userId = locals.user.id;
-	const workspaceSlug = params.workspace;
-
-	const [
-		{ data: workspaceData },
-		{ data: issueData },
-		{ data: fullSubIssues },
-		{ data: fullAssignee }
-	] = await Promise.all([
-		supabaseAdmin
-			.from('workspaces')
-			.select('id')
-			.eq('slug', workspaceSlug)
-			.maybeSingle(),
-		supabaseAdmin
-			.from('issues')
-			.select('id, name, status')
-			.eq('id', issueId)
-			.maybeSingle(),
-		supabaseAdmin
-			.from('issues')
-			.select('id, name, status, parent_id')
-			.eq('parent_id', issueId)
-			.order('updated_at', { ascending: false }),
-		supabaseAdmin.from('users').select('id, name').eq('id', userId).maybeSingle()
-	]);
-
-	const workspaceId = workspaceData?.id ?? null;
-	if (!workspaceId) {
-		return { issue: null, subIssues: [], assignee: null, userId };
-	}
-
-	const issue = issueData
-		? {
-				...issueData,
-				description: null,
-				status: allowedStatuses.has(issueData.status) ? issueData.status : 'todo'
-			}
-		: null;
-
-	const subIssues = (fullSubIssues ?? []).map((s) => ({
-		...s,
-		status: allowedStatuses.has(s.status) ? s.status : 'todo'
-	}));
-
-	return { issue, subIssues, assignee: fullAssignee, userId };
+	return {};
 };
