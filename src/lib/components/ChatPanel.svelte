9fc3bbb61d5d338330d336aeecabd7a8d0956375
Andrew Chang <21andrewch@gmail.com>
2026-04-01 07:09:04 -0700

    feat: got rid of top bar and x

--- src/lib/components/ChatPanel.svelte
+++ src/lib/components/ChatPanel.svelte
@@ -77,33 +77,11 @@
 </script>
 
 <div class="flex h-full flex-col">
-	<div class="flex items-center justify-between border-b border-neutral-200 px-6 py-3">
-		<div class="flex items-center gap-2 text-neutral-700"></div>
-		<button
-			on:click={() => dispatch('close')}
-			class="ml-3 shrink-0 text-neutral-400 transition hover:text-neutral-600"
-			aria-label="Close panel"
-			type="button"
-		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="16"
-				height="16"
-				fill="currentColor"
-				viewBox="0 0 16 16"
-			>
-				<path
-					d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"
-				/>
-			</svg>
-		</button>
-	</div>
-
 	<div class="flex-1 overflow-y-auto px-6 py-4">
 		<MessageThread messages={$chatMessages} {tenant} />
 	</div>
 
-	<div class="border-t border-neutral-200 px-6 py-4">
+	<div class="px-6 py-4">
 		<div
 			class="rounded-xl border border-neutral-100 bg-white px-4 py-3 shadow-[0_1px_3px_rgba(0,0,0,0.06)]"
 		>
